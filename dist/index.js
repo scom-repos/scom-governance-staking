dec37ad40b16a6541a251c79b28f2bdc4016e6e3
ojochau <ojochau@instajs.com>
2023-09-22 14:43:00 +0800

    Update CoreAddress: add Polygon and Arbitrum

--- dist/index.js
+++ dist/index.js
@@ -44,6 +44,14 @@ define("@scom/scom-governance-staking/store/core.ts", ["require", "exports"], fu
             GovToken: "0x45eee762aaeA4e5ce317471BDa8782724972Ee19",
             OAXDEX_Governance: "0xDfC070E2dbDAdcf892aE2ed2E2C426aDa835c528",
         },
+        137: {
+            GovToken: "0x29E65d6f3e7a609E0138a1331D42D23159124B8E",
+            OAXDEX_Governance: "0x5580B68478e714C02850251353Cc58B85D4033C3",
+        },
+        80001: {
+            GovToken: "0xb0AF504638BDe5e53D6EaE1119dEd13411c35cF2",
+            OAXDEX_Governance: "0x198b150E554F46aee505a7fb574F5D7895889772",
+        },
         43113: {
             GovToken: "0x27eF998b96c9A66937DBAc38c405Adcd7fa5e7DB",
             OAXDEX_Governance: "0xC025b30e6D4cBe4B6978a1A71a86e6eCB9F87F92",
@@ -51,6 +59,14 @@ define("@scom/scom-governance-staking/store/core.ts", ["require", "exports"], fu
         43114: {
             GovToken: "0x29E65d6f3e7a609E0138a1331D42D23159124B8E",
             OAXDEX_Governance: "0x845308010c3b699150cdd54dcf0e7c4b8653e6b2",
+        },
+        42161: {
+            GovToken: "0x29E65d6f3e7a609E0138a1331D42D23159124B8E",
+            OAXDEX_Governance: "0x5580B68478e714C02850251353Cc58B85D4033C3",
+        },
+        421613: {
+            GovToken: "0x5580B68478e714C02850251353Cc58B85D4033C3",
+            OAXDEX_Governance: "0x6f460B0Bf633E22503Efa460429B0Ab32d655B9D",
         },
     };
 });
@@ -389,7 +405,7 @@ define("@scom/scom-governance-staking/api.ts", ["require", "exports", "@ijstech/
 define("@scom/scom-governance-staking/formSchema.ts", ["require", "exports", "@scom/scom-network-picker"], function (require, exports, scom_network_picker_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    const chainIds = [1, 56, 137, 250, 97, 80001, 43113, 43114];
+    const chainIds = [1, 56, 137, 250, 97, 80001, 43113, 43114, 42161, 421613];
     const networks = chainIds.map(v => { return { chainId: v }; });
     exports.default = {
         dataSchema: {
